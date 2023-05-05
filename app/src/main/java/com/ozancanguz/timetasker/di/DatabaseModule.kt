5a999400dff8baefa1e23e4683dae3c6a92c791a
Ozan <ozancangu24@gmail.com>
2023-05-05 18:00:35 +0300

    -DatabaseModule.kt created

---
+++ app/src/main/java/com/ozancanguz/timetasker/di/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.ozancanguz.timetasker.di
+
+import android.content.Context
+import androidx.room.Room
+import com.ozancanguz.timetasker.data.db.TaskDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        TaskDatabase::class.java,
+        "taskList_database"
+    ).build()
+
+
+    @Singleton
+    @Provides
+    fun provideDao(database: TaskDatabase) = database.taskDao()
+}
