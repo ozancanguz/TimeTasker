35b7cdd113977d1337d43cea4c3bdda8031ee54f
Ozan <ozancangu24@gmail.com>
2023-05-05 17:56:49 +0300

    -TaskDao.kt created

---
+++ app/src/main/java/com/ozancanguz/timetasker/data/db/TaskDao.kt
@@ -0,0 +1,22 @@
+package com.ozancanguz.timetasker.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.ozancanguz.timetasker.data.model.Task
+
+
+@Dao
+interface TaskDao {
+
+    @Query("select * from tasklist_table order by id asc")
+    fun getAllTaskList(): LiveData<List<Task>>
+
+    @Insert
+    suspend fun insertTask(task: Task)
+
+    @Delete
+    suspend fun deleteExpense(task: Task)
+}
--- app/src/main/java/com/ozancanguz/timetasker/data/model/Task.kt
+++ app/src/main/java/com/ozancanguz/timetasker/data/model/Task.kt
@@ -1,7 +1,12 @@
 package com.ozancanguz.timetasker.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasklist_table")
 data class Task(
 
+    @PrimaryKey(autoGenerate = true)
     var id:Int=0,
     var task:String
 )
